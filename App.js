67efd30e71668e749c22c6b0b51dd634cbe5d43d
John Henly A. Montera <monterahens@gmail.com>
2021-10-17 09:48:32 +0800

    update lang tayo wala nang homescreen

--- App.js
+++ App.js
@@ -4,7 +4,7 @@
 // Gmail: monterahens@gmail.com 
 import * as React from 'react';
 import { useState } from 'react';
-import { View, Text, Image, ImageBackground,TouchableOpacity, StyleSheet, TextInput, ToastAndroid } from 'react-native';
+import { View, Text, Image, ImageBackground,TouchableOpacity, StyleSheet, TextInput, ToastAndroid, Button } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
@@ -16,8 +16,8 @@ import BreadSearch from './scr/BreadSearch.js';
 import BreadDelete from './scr/BreadDelete.js';
 import BreadList from './scr/BreadList.js';
 import BreadAbout from './scr/BreadAbout';
-import HomeScreen from './scr/HomeScreen.js';
 
+import bg2 from './assets/bg2.png';
 import logo from './assets/logo.png';
 import cafe from './assets/cafe.gif';
 import menubg from './assets/menu.png';
@@ -373,6 +373,102 @@ export default function App() {
             <Stack.Screen name="HomeSecond" component={Bottomtabnav}/>
           </Stack.Navigator>
       </NavigationContainer>
+    );
+  }
+
+function HomeScreen({ navigation }) {
+
+    return (
+      <View>  
+       <ImageBackground 
+                  source={bg2} 
+                  resizeMode="cover" 
+                  style={{
+                  height: '100%',
+                  width: '100%'
+       }}>
+      
+        <Text style={{
+          fontSize: 35,
+          fontFamily: 'sans-serif',
+          fontWeight: 'bold',
+          fontStyle: "italic",
+          position: 'absolute',
+          alignSelf:'center',
+          top: 300,
+          color: 'white',
+          textShadowColor:'#0c0d0e',
+          textShadowOffset:{width: 10, height: 10},
+          textShadowRadius:20,
+          textAlign: 'center',
+        }}> BAKERY {"\n"} ORDERING SYSTEM </Text>
+  
+          <Text style={{
+          fontSize: 20,
+          fontFamily: 'sans-serif',
+          fontWeight: 'bold',
+          fontStyle: "italic",
+          position: 'absolute',
+          alignSelf:'center',
+          top: 395,
+          color: 'white',
+          textShadowColor:'#0c0d0e',
+          textShadowOffset:{width: 10, height: 10},
+          textShadowRadius:20,
+          textAlign: 'center'
+        }}> System Database for Delivery Management™ </Text>
+  
+          <Image 
+           source= {logo}
+           style={{ 
+           height: 180,
+           width: 180,
+           position: 'absolute',
+           alignSelf:'center',
+           top: 100,
+           resizeMode: 'center',
+           }}/>
+  
+        <View style={{ 
+          flexDirection: 'row', 
+          top: '120%', 
+          justifyContent: 'space-evenly',
+          }}>
+  
+        <Button
+        color="#382624"
+          title={"Insert"}
+          onPress={() => navigation.navigate('Insert')}
+        />
+  
+        <Button
+        color="#382624"
+          title={"Search"}
+          onPress={() => navigation.navigate('Search')}
+        />
+  
+        <Button
+        color="#382624"
+          title={"Delete"}
+          onPress={() => navigation.navigate('Delete')}
+        />
+  
+        <Button
+          color="#382624"
+          title={"Bread List"}
+          onPress={() => navigation.navigate('List')}
+        />
+  
+        <Button
+          color="#382624"
+          title={"Menu"}
+          onPress={() => navigation.navigate('About')}
+        />
+      
+        </View>
+        </ImageBackground>
+        </View>
+        
     );
   }
 
