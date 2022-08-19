2ddc8ff6d43c46176f963456ebaf4580ee9ac24d
John Henly A. Montera <monterahens@gmail.com>
2022-08-19 13:58:21 +0800

    Initial Commit

--- scr/BreadInsert.js
+++ scr/BreadInsert.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import { View, TextInput, Button, StyleSheet, ImageBackground, Text,Platform } from 'react-native';
+import { View, TextInput, Button, StyleSheet, ImageBackground, Text,Platform, Alert } from 'react-native';
 import bg from '../assets/bg.jpg';
 import ModalDropdown from 'react-native-modal-dropdown';
 import DateTimePicker from '@react-native-community/datetimepicker';
--- scr/BreadSearch.js
+++ scr/BreadSearch.js
@@ -10,7 +10,8 @@ import {
     View, 
     TextInput,
     Button,
-    ImageBackground
+    ImageBackground,
+    Alert
 } from 'react-native';
 import bg from '../assets/bg.jpg';
 
@@ -31,25 +32,22 @@ export default class BreadSearch extends Component {
 
     SearchRecord=()=>
     {
-        var ID=this.state.ID;
-
-        if(ID.length==0)
+        var ID = this.state.ID;
+        
+        if(ID.length == 0)
         {
-            alert("Required Field is Missing");
+            Alert.alert("Search Info","Required Field is Missing");
         }
         else
         {
             var SearchAPIURL="http://10.0.2.2:80/bakery2/search.php";
-
             var header={
                     'Accept':'application/json',
                     'Content-Type':'application/json'
             };
-
             var Data={
                 ID:ID
             };
-
             fetch(
                 SearchAPIURL,
                 {
@@ -72,6 +70,60 @@ export default class BreadSearch extends Component {
             })
         }
     }
+
+    UpdateRecord=()=>
+    {
+        var ID = this.state.ID;
+        var price = this.state.price;
+        var ordered = this.state.ordered;
+        var pieces = this.state.pieces;
+        var Type_ = this.state.Type_;
+        var address = this.state.address;
+
+
+        if(ID == null)
+        {
+            console.log(ID);
+            Alert.alert("Update Info","Required Field is Missing");
+        }
+        else
+        {
+            var InsertAPIURL = "http://10.0.2.2:80/bakery2/update.php";
+            var headers={
+                'Accept':'application/json',
+                'Content-Type':'application.json'
+            };
+
+            var Data={
+                ID:ID,
+                price:price,
+                ordered:ordered,
+                pieces:pieces,
+                Type_:Type_,
+                address:address
+            };
+
+            fetch(InsertAPIURL,
+                {
+                    method:'POST',
+                    headers:headers,
+                    body: JSON.stringify(Data)
+                }
+                )
+                .then((response)=>response.json())
+                .then((response)=>
+                {
+                    alert(response[0].Message);
+                })
+                .catch((error)=>
+                {
+                    alert("Error"+error);
+                })
+
+        }
+    }
+    
+
     render()
     {
     return (
@@ -88,7 +140,6 @@ export default class BreadSearch extends Component {
         fontFamily: 'sans-serif',
         fontWeight: 'bold',
         fontStyle: "italic",
-        position: 'absolute',
         top: 65,
         left: 8,
         color: 'white',
@@ -102,8 +153,7 @@ export default class BreadSearch extends Component {
         fontFamily: 'sans-serif',
         fontWeight: 'bold',
         fontStyle: "italic",
-        position: 'absolute',
-        top: 110,
+        top: 65,
         left: 13,
         color: 'white',
         textShadowColor:'#0c0d0e',
@@ -120,6 +170,9 @@ export default class BreadSearch extends Component {
                 style={styles.txtStyle}
                 onChangeText={ID=>this.setState({ID})}
             />
+
+            <Text> </Text>
+
             <Button
                 color="#382624"
                 title={"Find Record"}
@@ -127,33 +180,59 @@ export default class BreadSearch extends Component {
             />
 
             <Text> </Text>
-            <Text> </Text>
-            <Text> </Text>
 
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 13, marginBottom: 5}}>Price</Text>
+            
             <TextInput
             style={styles.txtStyle}
-            value={"Price: "+this.state.price}
+            value={this.state.price}
+            keyboardType={"numeric"}
+            onChangeText={price=>this.setState({price})}
             />
 
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 13, marginBottom: 5}}>Ordered</Text>
             <TextInput
             style={styles.txtStyle}
-            value={"Ordered: "+this.state.ordered}
+            value={this.state.ordered}
+            onChangeText={ordered=>this.setState({ordered})}
             />
 
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 13, marginBottom: 5}}>Pieces</Text>
             <TextInput
             style={styles.txtStyle}
-            value={"Pieces: "+this.state.pieces}
+            value={this.state.pieces}
+            keyboardType={"numeric"}
+            onChangeText={pieces=>this.setState({pieces})}
             />
 
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 13, marginBottom: 5}}>Type</Text>
             <TextInput
             style={styles.txtStyle}
-            value={"Delivery Type: "+this.state.Type_}
+            value={this.state.Type_}
+            onChangeText={Type_=>this.setState({Type_})}
             />
 
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 13, marginBottom: 5}}>Address</Text>
             <TextInput
             style={styles.txtStyle}
-            value={"Address: "+this.state.address}
+            value={this.state.address}
+            onChangeText={address=>this.setState({address})}
             />
+        
+
+            <View style={styles.containerbutton}>
+
+            <Button
+                color="#382624"
+                title={"Update"}
+                onPress={this.UpdateRecord}
+                style={styles.containerbutton}
+            />
+
+            </View>
+
+
+
 
 
         </View>
@@ -169,8 +248,7 @@ const styles=StyleSheet.create({
     {
         flex:1,
         padding:20,
-        marginTop:20,
-        top: '20%',
+        top: '8%',
         
     },
 
@@ -183,7 +261,13 @@ const styles=StyleSheet.create({
         padding: 10,
         borderRadius: 5
     },
-   
+
+    containerbutton:
+    {
+        marginTop: 10,
+        alignSelf: 'center',
+        width: '50%'
+    },
 
 
 
